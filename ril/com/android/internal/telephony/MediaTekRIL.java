ef043e594c0d16b5b79bbd893f9dddf50bf27895
cakehonolulu <cakehonolulu@protonmail.com>
2017-10-29 22:20:46 +0100

    MediaTekRIL: Update year

--- ril/com/android/internal/telephony/MediaTekRIL.java
+++ ril/com/android/internal/telephony/MediaTekRIL.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The OmniROM Project <http://www.omnirom.org>
+ * Copyright (C) 2017 The LineageOS Project <http://www.omnirom.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
